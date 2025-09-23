5801aae7fd0596f3ba6c69e0c14747ac5a041085
nguyen van bac <nguyenbac29305@gmail.com>
2025-09-23 21:03:03 +0700

    show sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,15 @@
-
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index",{
-        pageTitle: "Trang sản phẩm"
-    });
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
+  const newProducts = products.map((item) => {
+    item.priceNew = (item.price * (1 - item.discountPercentage / 100)).toFixed(2);
+    return item;
+  });
+  res.render("client/pages/products/index", {
+    pageTitle: "Trang sản phẩm",
+    products: newProducts,
+  });
 };
