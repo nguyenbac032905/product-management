8de8757b68b01094b072d79f8449e8e3e223568d
nguyen van bac <nguyenbac29305@gmail.com>
2025-09-25 20:06:50 +0700

    phân trang

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -18,12 +18,29 @@ module.exports.index = async (req,res) => {
         find.title = objectSearch.regex;
     };
 
-    const products = await Product.find(find);
+    //pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItem: 4
+    };
+    if(req.query.page){
+        objectPagination.currentPage = parseInt(req.query.page);
+    };
+    objectPagination.skip = (objectPagination.currentPage - 1)*objectPagination.limitItem;
+
+    //tính số trang
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+    objectPagination.totalPage = totalPage;
+    //pagination
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
     res.render("admin/pages/products/index",{
         pageTitle: "Trang sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,40 +1,60 @@
 // button status
 const buttonStatus = document.querySelectorAll("[button-status]");
 
-if(buttonStatus.length > 0){
-    //lấy url hiện tại
-    let url = new URL(window.location.href);
+if (buttonStatus.length > 0) {
+  //lấy url hiện tại
+  let url = new URL(window.location.href);
 
-    buttonStatus.forEach(button => {
-        button.addEventListener("click", () =>{
-            const status = button.getAttribute("button-status");
-            if(status){
-                //nếu chọn status thì thêm vào url
-                url.searchParams.set("status", status);
-            }else{
-                //
-                url.searchParams.delete("status");
-            }
-            //chuyển hướng trang
-            window.location.href = url.href;
-        })
-    })
-};
+  buttonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if (status) {
+        //nếu chọn status thì thêm vào url
+        url.searchParams.set("status", status);
+      } else {
+        //
+        url.searchParams.delete("status");
+      }
+      //chuyển hướng trang
+      window.location.href = url.href;
+    });
+  });
+}
 
 //form search
 const formSearch = document.querySelector("#form-search");
-if(formSearch){
-    let url = new  URL(window.location.href);
-    formSearch.addEventListener("submit", (e) => {
-        e.preventDefault();
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        const keyword = e.target[0].value;
-        if(keyword){
-            url.searchParams.set("keyword",keyword);
-        }else{
-            url.searchParams.delete("keyword");
-        }
-        
-        window.location.href = url.href;
-    })
+    const keyword = e.target[0].value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+
+    window.location.href = url.href;
+  });
 }
+
+//pagination
+const buttonPagi = document.querySelectorAll("[button-pagination]");
+if (buttonPagi) {
+    const url = new URL(window.location.href);
+    buttonPagi.forEach((item) => {
+
+    item.addEventListener("click", () => {
+      const page = item.getAttribute("button-pagination");
+
+      if(page){
+        url.searchParams.set("page", page);
+      }else{
+        url.searchParams.delete("page");
+      }
+
+      window.location.href = url.href;
+    });
+  });
+};
