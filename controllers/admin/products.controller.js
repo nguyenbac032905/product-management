3a3c6da2b9f5391e2e743363cb061746f7816b84
nguyen van bac <nguyenbac29305@gmail.com>
2025-09-30 18:23:37 +0700

    upload ảnh

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -107,6 +107,7 @@ module.exports.createPost = async (req,res) => {
     req.body.price = parseInt(req.body.price);
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
     if(req.body.position == ""){
         const countProducts = await Product.countDocuments()
         req.body.position = countProducts + 1;
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/products.controller");
+//import hàm storageMulter
+const storageMulter = require("../../helpers/storageMulter");
+const multer  = require('multer');
+//nơi lưu ảnh
+const upload = multer({storage: storageMulter()});
 
 router.get("/",controller.index);
 router.patch("/change-status/:status/:id",controller.changeStatus);
 router.patch("/change-multi",controller.changeMulti);
 router.delete("/delete/:id",controller.deleteProduct);
 router.get("/create",controller.create);
-router.post("/create",controller.createPost);
+router.post("/create",upload.single('thumbnail'),controller.createPost);
 module.exports = router;
