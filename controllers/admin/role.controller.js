aa04e6cfc2452aac830613d05d7dc526b1259ac2
nguyen van bac <nguyenbac29305@gmail.com>
2025-10-07 20:11:30 +0700

    trang tạo quyền và in ra giao diện

---
+++ controllers/admin/role.controller.js
@@ -0,0 +1,22 @@
+const Role = require("../../models/role.modle");
+const systemConfig = require("../../config/system");
+module.exports.index = async (req,res) => {
+    let find = {
+        deleted: false
+    };
+    const records = await Role.find(find);
+    res.render("admin/pages/roles/index",{
+        pageTitle: "Nhóm quyền",
+        records: records
+    });
+};
+module.exports.create = async (req,res) => {
+    res.render("admin/pages/roles/create",{
+        pageTitle: "Tạo nhóm quyền"
+    });
+};
+module.exports.createPost = async (req,res) => {
+    const record = new Role(req.body);
+    await record.save();
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
