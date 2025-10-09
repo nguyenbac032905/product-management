24bf86799fd6cc7483790e1f805dec11f9ecc30b
nguyen van bac <nguyenbac29305@gmail.com>
2025-10-09 17:29:25 +0700

    tạo model account

---
+++ models/account.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+const generate = require("../helpers/generate");
+mongoose.plugin(slug);
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    token: {
+        type: String,
+        default: generateRandomString(20)
+    },
+    phone: String,
+    avatar: String,
+    role_id: String,
+    status: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deleteAt: Date
+},{
+    timestamps: true
+});
+const Account = mongoose.model("Account",accountSchema,"accounts");
+module.exports = Account;
