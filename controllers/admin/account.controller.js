4b868f520f4f138cf27f0e467313087418fc39e1
nguyen van bac <nguyenbac29305@gmail.com>
2025-10-09 19:55:27 +0700

    cập nhật tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -49,3 +49,48 @@ module.exports.createPost = async (req,res)=>{
     }
     res.redirect(res.get("Referer") || `${systemConfig.prefixAdmin}/accounts/create`)
 };
+module.exports.edit = async (req,res) => {
+    try {
+        const id = req.params.id;
+        let find = {
+            deleted: false,
+            _id: id
+        };
+        const account = await Account.findOne(find);
+        const roles = await Role.find({
+            deleted: false,
+        });
+        res.render("admin/pages/accounts/edit",{
+            pageTitle: "Chỉnh sửa Tài Khoản",
+            data: account,
+            roles: roles
+        })
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+    }
+};
+module.exports.editPatch = async (req,res) => {
+    const id = req.params.id;
+    try {
+        const emailExist = await Account.findOne({
+            email: req.body.email,
+            deleted: false,
+            _id: {$ne: id} //tìm ra tất cả các id ngoại trừ id mình truyền vào (not equals)
+        });
+        if(emailExist){
+            req.flash("error","email đã tồn tại");
+        }else{
+            if(req.body.password){
+                req.body.password = md5(req.body.password);
+            }else{
+                delete req.body.password;
+            }
+
+            await Account.updateOne({_id: id},req.body);
+            req.flash("success","cập nhật thành công");
+        }
+    } catch (error) {
+        req.flash("success","cập nhật Thất Bại");
+    }
+    res.redirect(res.get("Referer") || `${systemConfig.prefixAdmin}/accounts/edit/${req.params.id}`)
+};
--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -16,4 +16,19 @@ module.exports.createPost = (req,res,next) => {
     }
     //sử dụng next để chuyển sang hàm phía sau trong routes
     next();
+};
+
+module.exports.editPatch = (req,res,next) => {
+    if(!req.body.fullName){
+        req.flash("error","vui lòng nhập họ tên");
+        res.redirect(req.get("Referer") || "/admin/products");
+        return;
+    }
+    if(!req.body.email){
+        req.flash("error","vui lòng nhập email");
+        res.redirect(req.get("Referer") || "/admin/products");
+        return;
+    }
+    //sử dụng next để chuyển sang hàm phía sau trong routes
+    next();
 }
