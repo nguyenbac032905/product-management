e134d5835c596c4025caa1c822d966b3af4dd330
nguyen van bac <nguyenbac29305@gmail.com>
2025-10-16 18:52:49 +0700

    trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,8 +1,30 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/product");
 
 module.exports.index = async (req,res) =>{
+    const cartId = req.cookies.cartId;
+    const cart = await Cart.findOne({_id: cartId});
+    const products = [];
+
+    if(cart.products.length > 0){
+        const productsInCart = cart.products;
+        for(item of productsInCart){
+            const product = await Product.findOne({
+                _id: item.product_id
+            }).select("title thumbnail slug price discountPercentage");
+            product.quantity = item.quantity;
+            products.push(product);
+        }
+    }
+
+    const newProducts = productHelper.newPrice(products);
+    newProducts.cartTotalPrice = newProducts.reduce((sum,item) => {
+        return sum + item.quantity*item.priceNew;
+    },0);
     res.render("client/pages/cart/index",{
-        pageTitle: "Giỏ hàng"
+        pageTitle: "Giỏ hàng",
+        products: newProducts
     });
 };
 module.exports.addPost = async (req,res) => {
