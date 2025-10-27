cec4a2a767bb8741ef25545e786c7bf8e5baa0a5
nguyen van bac <nguyenbac29305@gmail.com>
2025-10-27 08:57:53 +0700

    tạo collection roomchat

---
+++ models/room-chat.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const roomChatSchema = new mongoose.Schema({
+    title: String,
+    avatar: String,
+    typeRoom: String,
+    status: String,
+    users: [
+        {
+            user_id: String,
+            role: String
+        }
+    ],
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deleteAt: Date
+},{
+    timestamps: true
+});
+const RoomChat = mongoose.model("RoomChat",roomChatSchema,"rooms-chat");
+module.exports = RoomChat;
